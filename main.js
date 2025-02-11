59ca53654dda8bf3aa7daa8b12ca3fb590c694aa
AILyricEditor <ailyriceditor@gmail.com>
2025-02-11 18:00:14 -0500

    Made more hover animations

--- main.js
+++ main.js
@@ -56,10 +56,18 @@ function addCard() {
 				<input type="text" class="input license-input" maxlength="8" value="15E28Y">
 				<img class="license-image" src="assets/Blank-License-Plate.png" alt="License Plate">
 			</div>
+
+			<div class="bottom-row">
+				<button class="delete-card">
+					<p>Delete</p>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+				</button>
+			</div>
 		</section>
 	`;
 	setEvents();
 	adjustFontSize();
+	adjustNamePositons();
 	editButton();
 	expandCard();
 	// closeCard();
@@ -150,7 +158,7 @@ function editButton() {
 				}
 			});
 			// Toggle 'editMode' on the clicked button
-			edit.classList.toggle("editMode");
+			edit.closest(".card").classList.toggle("editMode");
 		});
 	});
 }
@@ -163,6 +171,8 @@ function expandCard() {
 				document.querySelectorAll(".card").forEach(el => el.classList.remove("expanded"));
 				this.parentElement.classList.add("expanded");
 				toggleOverlay();
+				adjustFontSize();
+				adjustNamePositons();
 			}
 		});
 	});
